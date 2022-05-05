74e5b00a4921b71a3cda5b588f563744bbb489e2
dprieeto <52801820+dprieeto@users.noreply.github.com>
2022-05-05 10:16:06 +0200

    Add files via upload

---
+++ 1º año/Fundamentos de programacion/Practicas/Guion 2/Repetitivas/ejercicio 7 guion 2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class primo
+{
+    int n;
+public:
+    void cargar();
+    bool esprimo();
+    void listaprimos();
+};
+void primo::cargar()
+{
+    cout<<"\t\t\tIntroduce un numero entero: ";cin>>n;
+}
+bool primo::esprimo()
+{
+    int primo1;
+    int i=2;
+    do
+    {
+        if (i!=n && n%i==0)
+            primo1=false;
+            i++;
+    }while (i<=n/2 and primo1);
+    return primo1;
+}
+int main()
+{
+    system("pause");
+    system("cls");
+    system("color 0a");
+    primo ob;
+    ob.cargar();
+    cout<<"El numero elegido "<<(ob.esprimo()?"si":"no")<<" es primo."<<endl;
+    system("pause");
+    system("color 06");
+    return 0;
+}
---
+++ 1º año/Fundamentos de programacion/Practicas/Guion 2/Repetitivas/ejercicio 8 guion 2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+class numero{
+    int a;
+    public:
+        void informacion();
+        long factorial();
+};
+void numero::informacion(){
+    cout<<"Vamos a calcular el factorial de un numero:\n";
+    do
+    {
+        cout<<"Introduce un numero entero positivo menor que 20: ";cin>>a;
+    }
+    while (a<0 or a>=20);
+}
+long numero::factorial(){
+    long factorial = 1;
+
+	for(int i = 1; i <= a; i++)
+		factorial *= i;
+
+	return factorial;
+}
+
+int main()
+{
+    numero ob;
+    ob.informacion();
+    cout<<"El factorial del numero elegido es: "<<ob.factorial();
+    return 0;
+}
